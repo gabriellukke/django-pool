10c8de20f882c714513dd161ef2b6d73380d9304
gabriellukke <gabriellukke@outlook.com>
2022-09-22 15:02:54 -0300

    feat: question vote view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,3 +12,7 @@ def details(request, question_id):
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
