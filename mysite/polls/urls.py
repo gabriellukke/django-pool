246b5fe8bf2657fc3b3196c44697efab5116718c
gabriellukke <gabriellukke@outlook.com>
2022-09-22 18:56:32 -0300

    refact: use generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,12 +4,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    # /polls/
-    path('', views.index, name='index'),
-    # /polls/5/
-    path('<int:question_id>/', views.details, name='details'),
-    # /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailsView.as_view(), name='details'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,26 +1,28 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def details(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/details.html', {'question': question})
+class DetailsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
