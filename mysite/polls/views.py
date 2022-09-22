4e4d0d6cad629fd97dd45882380f27f7fdadf5dd
gabriellukke <gabriellukke@outlook.com>
2022-09-22 15:32:59 -0300

    feat: format question text

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def details(request, question_id):
